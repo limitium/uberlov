d43c6bada429b8290203acabf3ac3652907f15a9
Limitium <Limitium@50e93683-1401-4f79-b91e-9bcc27dc36b6>
2009-10-14 21:52:29 +0000

    closed #1 sped 2 hours

---
+++ config/ProjectConfiguration.class.php
@@ -0,0 +1,12 @@
+<?php
+
+require_once 'D:\\WebServers\\symfony-1.3.0ALPHA2\\lib/autoload/sfCoreAutoload.class.php';
+sfCoreAutoload::register();
+
+class ProjectConfiguration extends sfProjectConfiguration
+{
+  public function setup()
+  {
+    $this->enablePlugins('sfDoctrinePlugin');
+  }
+}
