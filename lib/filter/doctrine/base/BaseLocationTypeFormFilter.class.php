1d2c9b95451254aa4139c239755f71ca092c9061
Limitium <Limitium@50e93683-1401-4f79-b91e-9bcc27dc36b6>
2009-10-23 12:59:58 +0000

    closed #3 spend 16

---
+++ lib/filter/doctrine/base/BaseLocationTypeFormFilter.class.php
@@ -0,0 +1,44 @@
+<?php
+
+/**
+ * LocationType filter form base class.
+ *
+ * @package    HT
+ * @subpackage filter
+ * @author     Your name here
+ * @version    SVN: $Id$
+ */
+class BaseLocationTypeFormFilter extends BaseFormFilterDoctrine
+{
+  public function setup()
+  {
+    $this->setWidgets(array(
+      'name' => new sfWidgetFormFilterInput(array('with_empty' => false)),
+    ));
+
+    $this->setValidators(array(
+      'name' => new sfValidatorPass(array('required' => false)),
+    ));
+
+    $this->widgetSchema->setNameFormat('location_type_filters[%s]');
+
+    $this->errorSchema = new sfValidatorErrorSchema($this->validatorSchema);
+
+    $this->setupInheritance();
+
+    parent::setup();
+  }
+
+  public function getModelName()
+  {
+    return 'LocationType';
+  }
+
+  public function getFields()
+  {
+    return array(
+      'id'   => 'Number',
+      'name' => 'Text',
+    );
+  }
+}
