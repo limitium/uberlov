5d7adcc193821ceb9836fb516dea0c58a957531b
Sergey Belov <limitium@users.noreply.github.com>
2010-03-29 11:31:09 +0000

    added profit module removed routes changed location cre8ion

---
+++ lib/filter/doctrine/ProfitSyleFormFilter.class.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * ProfitSyle filter form.
+ *
+ * @package    HT
+ * @subpackage filter
+ * @author     Your name here
+ * @version    SVN: $Id: sfDoctrineFormFilterTemplate.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class ProfitSyleFormFilter extends BaseProfitSyleFormFilter
+{
+  public function configure()
+  {
+  }
+}
