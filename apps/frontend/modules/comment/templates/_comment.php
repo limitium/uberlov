f028a55e572938bc738d4dbf5c5362d08c8f66b0
limitium <limitium@50e93683-1401-4f79-b91e-9bcc27dc36b6>
2010-04-05 15:26:59 +0000

    link to profie added

--- apps/frontend/modules/comment/templates/_comment.php
+++ apps/frontend/modules/comment/templates/_comment.php
@@ -1,9 +1,9 @@
 <div id="comment<?php echo $comment->getId(); ?>" class="comment tree indent_<?php echo $comment->getLevel(); ?>">
     <div class="commentMessage"><?php echo $comment->getMessage(); ?></div>
     <div class="commentBar">
-        written asdasd,
+        written <?php echo link_to($comment->getCommentBy()->getNickName(),'profile/show?id='.$comment->getCommentBy()->getId()); ?>,
         <?php echo $comment->getDateTimeObject('created_at')->format('d.m.Y') ?> <?php echo $comment->getDateTimeObject('created_at')->format('H.i') ?>
-        | <a href="" class="commentReply">answer</a>
+        | <a href="" class="commentReply">answer</a>        
         <?php IF($comment->getLevel()>1): ?>
         <a href="" class="commentShowParent" replyTo="<?php echo $comment->getParent(); ?>">&uarr;</a>
         <?php ENDIF; ?>
