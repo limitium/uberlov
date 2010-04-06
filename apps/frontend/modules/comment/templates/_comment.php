a30d0d37a1d5ed6ac571cf1b5b5492e560f571bd
Sergey Belov <limitium@users.noreply.github.com>
2010-04-06 18:23:38 +0000

    rate it all))) spend 5 hours

--- apps/frontend/modules/comment/templates/_comment.php
+++ apps/frontend/modules/comment/templates/_comment.php
@@ -1,6 +1,7 @@
 <div id="comment<?php echo $comment->getId(); ?>" class="comment tree indent_<?php echo $comment->getLevel(); ?>">
     <div class="commentMessage"><?php echo $comment->getMessage(); ?></div>
     <div class="commentBar">
+        <?php include_partial('vote/vote', array('obj' => $comment,'objType' => 'comment')); ?>
         written <?php echo link_to($comment->getCommentBy()->getNickName(),'profile/show?id='.$comment->getCommentBy()->getId()); ?>,
         <?php echo $comment->getDateTimeObject('created_at')->format('d.m.Y') ?> <?php echo $comment->getDateTimeObject('created_at')->format('H.i') ?>
         | <a href="" class="commentReply">answer</a>        
--- apps/frontend/modules/vote/templates/_vote.php
+++ apps/frontend/modules/vote/templates/_vote.php
@@ -1,9 +1,10 @@
+<?php $rating = $obj->getRating(); ?>
 <div class="rating" to="<?php echo $objType; ?>" id="rate<?php echo $obj->getId(); ?>">
-    <a class ="plus" href="">
+    <a class ="plus<?php echo $obj->plused?' voted':''; ?>" href="">
         <img src="/images/plus.png">
     </a>
-    <a class="minus" href="">
+    <a class="minus<?php echo $obj->minused?' voted':''; ?>" href="">
         <img src="/images/minus.png">
     </a>
-    <span>[<?php echo $obj->getRating(); ?>]</span>
+    <span>[<?php echo $rating; ?>]</span>
 </div>
