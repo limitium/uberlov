e5d5e640eb2521107b1132cdb08e9813040f59a5
Sergey Belov <limitium@users.noreply.github.com>
2010-05-16 20:55:02 +0000

    address done

--- apps/frontend/modules/location/actions/actions.class.php
+++ apps/frontend/modules/location/actions/actions.class.php
@@ -93,18 +93,16 @@ class locationActions extends sfActions {
     protected function processForm(sfWebRequest $request, sfForm $form) {
         $form->bind($request->getParameter($form->getName()));
         if ($form->isValid()) {
-            fb(123123);
             $addressData = (array) json_decode($form->getValue('address'));
-            fb($addressData);
             $address = new Address();
             $address->synchronizeWithArray($addressData);
-            fb($address->getCountry());
             if(!$form->isNew()) {
                 $form->getObject()->getAddress()->delete();
             }
             $location = $form->save();
             $location->setAddress($address);
-            return $location->save();
+            $location->save();
+            return $location;
         }
         return null;
     }
