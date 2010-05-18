49f90fda68afbbb992b9b1a3e2a6ef23e22272d7
limitium <limitium@50e93683-1401-4f79-b91e-9bcc27dc36b6>
2010-05-18 20:31:37 +0000

    hierarchy added to address parts

---
+++ lib/filter/doctrine/base/BaseAreaLowFormFilter.class.php
@@ -0,0 +1,47 @@
+<?php
+
+/**
+ * AreaLow filter form base class.
+ *
+ * @package    FISHERY
+ * @subpackage filter
+ * @author     Your name here
+ * @version    SVN: $Id: sfDoctrineFormFilterGeneratedTemplate.php 24171 2009-11-19 16:37:50Z Kris.Wallsmith $
+ */
+abstract class BaseAreaLowFormFilter extends BaseFormFilterDoctrine
+{
+  public function setup()
+  {
+    $this->setWidgets(array(
+      'name'       => new sfWidgetFormFilterInput(array('with_empty' => false)),
+      'country_id' => new sfWidgetFormDoctrineChoice(array('model' => $this->getRelatedModelName('Country'), 'add_empty' => true)),
+    ));
+
+    $this->setValidators(array(
+      'name'       => new sfValidatorPass(array('required' => false)),
+      'country_id' => new sfValidatorDoctrineChoice(array('required' => false, 'model' => $this->getRelatedModelName('Country'), 'column' => 'id')),
+    ));
+
+    $this->widgetSchema->setNameFormat('area_low_filters[%s]');
+
+    $this->errorSchema = new sfValidatorErrorSchema($this->validatorSchema);
+
+    $this->setupInheritance();
+
+    parent::setup();
+  }
+
+  public function getModelName()
+  {
+    return 'AreaLow';
+  }
+
+  public function getFields()
+  {
+    return array(
+      'id'         => 'Number',
+      'name'       => 'Text',
+      'country_id' => 'ForeignKey',
+    );
+  }
+}
