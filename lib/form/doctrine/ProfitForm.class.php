279dac8d0e98b09e9249305971b55ae88ed7a614
Sergey Belov <limitium@users.noreply.github.com>
2010-07-15 06:50:40 +0000

    fileds changed

--- lib/form/doctrine/ProfitForm.class.php
+++ lib/form/doctrine/ProfitForm.class.php
@@ -12,20 +12,25 @@ class ProfitForm extends BaseProfitForm {
 
     public function configure() {
         unset($this['created_at'],
-                $this['updated_at']
+                $this['updated_at'],
+                $this['created_by'],
+                $this['updated_by']
         );
 
         $this->widgetSchema['location_id'] = new sfWidgetFormInputHidden();
         $this->widgetSchema['id'] = new sfWidgetFormInputHidden();
         $this->widgetSchema['details'] = new sfWidgetFormInputHidden();
-//        $this->widgetSchema['date'] = new sfWidgetFormDate();
         $this->widgetSchema['date'] = new sfWidgetFormInputText();
+
+        $this->widgetSchema['best_weight'] = new sfWidgetFormInputText();
+        $this->widgetSchema['best_fish_id'] = new sfWidgetFormDoctrineChoice(array('model' => $this->getRelatedModelName('Fish'), 'add_empty' => true));
+
         $this->widgetSchema['qty'] = new sfWidgetFormInputText();
-        $this->widgetSchema['styles'] = new sfWidgetFormDoctrineChoice(array('model'=>'Style'));
-        $this->widgetSchema['fishes'] = new sfWidgetFormDoctrineChoice(array('model'=>'Fish'));
+        $this->widgetSchema['styles'] = new sfWidgetFormDoctrineChoice(array('model' => 'Style', 'add_empty' => true));
+        $this->widgetSchema['fishes'] = new sfWidgetFormDoctrineChoice(array('model' => 'Fish', 'add_empty' => true));
 
         $this->validatorSchema['location_id'] = new sfValidatorDoctrineChoice(array('model' => 'Location', 'required' => true));
-        $this->validatorSchema['date'] = new sfValidatorDate(array('date_format' => "/[0-2][0-9]\.[0-1][0-9]\.[0-9]{4}/",'with_time' =>false, 'required' => true, 'date_format_error' => 'd.m.Y'));
+        $this->validatorSchema['date'] = new sfValidatorDate(array('date_format' => "/[0-2][0-9]\.[0-1][0-9]\.[0-9]{4}/", 'with_time' => false, 'required' => true, 'date_format_error' => 'd.m.Y'));
         $this->validatorSchema['details'] = new sfValidatorString(array('required' => true));
 
         $this->validatorSchema['qty'] = new sfValidatorString();
