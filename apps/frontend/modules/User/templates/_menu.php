55a8cb5ac6a982e4267bd8e5c2b6a0af83eaddd9
Sergey Belov <limitium@users.noreply.github.com>
2010-07-20 11:26:25 +0000

    rename

--- apps/frontend/modules/User/templates/_menu.php
+++
@@ -1,9 +0,0 @@
-<div id="userMenu">
-    <ul>
-        <?php foreach ($menu as $menuItem): ?>
-        <li>
-            <?php echo link_to($menuItem['title'],$menuItem['url'])?>
-        </li>
-        <?php endforeach; ?>
-    </ul>
-</div>
