9a1e71be211061daae3f7092cd3ecb63ae7ec82b
Limitium <Limitium@50e93683-1401-4f79-b91e-9bcc27dc36b6>
2010-10-13 11:29:06 +0000

    tag suggest

--- apps/frontend/modules/talk/templates/_form.php
+++ apps/frontend/modules/talk/templates/_form.php
@@ -1,74 +1,46 @@
 <?php use_stylesheets_for_form($form) ?>
 <?php use_javascripts_for_form($form) ?>
 
-<form action="<?php echo url_for('talk/'.($form->getObject()->isNew() ? 'create' : 'update').(!$form->getObject()->isNew() ? '?id='.$form->getObject()->getId() : '')) ?>" method="post" <?php $form->isMultipart() and print 'enctype="multipart/form-data" ' ?>>
-<?php if (!$form->getObject()->isNew()): ?>
-<input type="hidden" name="sf_method" value="put" />
-<?php endif; ?>
-  <table>
-    <tfoot>
-      <tr>
-        <td colspan="2">
-          <?php echo $form->renderHiddenFields(false) ?>
-          &nbsp;<a href="<?php echo url_for('talk/index') ?>">Back to list</a>
-          <?php if (!$form->getObject()->isNew()): ?>
-            &nbsp;<?php echo link_to('Delete', 'talk/delete?id='.$form->getObject()->getId(), array('method' => 'delete', 'confirm' => 'Are you sure?')) ?>
-          <?php endif; ?>
-          <input type="submit" value="Save" />
-        </td>
-      </tr>
-    </tfoot>
-    <tbody>
-      <?php echo $form->renderGlobalErrors() ?>
-      <tr>
-        <th><?php echo $form['name']->renderLabel() ?></th>
-        <td>
-          <?php echo $form['name']->renderError() ?>
-          <?php echo $form['name'] ?>
-        </td>
-      </tr>
-      <tr>
-        <th><?php echo $form['message']->renderLabel() ?></th>
-        <td>
-          <?php echo $form['message']->renderError() ?>
-          <?php echo $form['message'] ?>
-        </td>
-      </tr>
-      <tr>
-        <th><?php echo $form['talk_section_id']->renderLabel() ?></th>
-        <td>
-          <?php echo $form['talk_section_id']->renderError() ?>
-          <?php echo $form['talk_section_id'] ?>
-        </td>
-      </tr>
-      <tr>
-        <th><?php echo $form['created_by']->renderLabel() ?></th>
-        <td>
-          <?php echo $form['created_by']->renderError() ?>
-          <?php echo $form['created_by'] ?>
-        </td>
-      </tr>
-      <tr>
-        <th><?php echo $form['updated_by']->renderLabel() ?></th>
-        <td>
-          <?php echo $form['updated_by']->renderError() ?>
-          <?php echo $form['updated_by'] ?>
-        </td>
-      </tr>
-      <tr>
-        <th><?php echo $form['created_at']->renderLabel() ?></th>
-        <td>
-          <?php echo $form['created_at']->renderError() ?>
-          <?php echo $form['created_at'] ?>
-        </td>
-      </tr>
-      <tr>
-        <th><?php echo $form['updated_at']->renderLabel() ?></th>
-        <td>
-          <?php echo $form['updated_at']->renderError() ?>
-          <?php echo $form['updated_at'] ?>
-        </td>
-      </tr>
-    </tbody>
-  </table>
+<form action="<?php echo url_for('talk/' . ($form->getObject()->isNew() ? 'create' : 'update') . (!$form->getObject()->isNew() ? '?id=' . $form->getObject()->getId() : '')) ?>" method="post" <?php $form->isMultipart() and print 'enctype="multipart/form-data" ' ?>>
+    <?php if (!$form->getObject()->isNew()): ?>
+        <input type="hidden" name="sf_method" value="put" />
+    <?php endif; ?>
+        <table>
+            <tfoot>
+                <tr>
+                    <td colspan="2">
+                    <?php echo $form->renderHiddenFields(false) ?>
+                    <input type="submit" value="Обсудить!" />
+                </td>
+            </tr>
+        </tfoot>
+        <tbody>
+            <?php echo $form->renderGlobalErrors() ?>
+                    <tr>
+                        <th><?php echo $form['name']->renderLabel() ?></th>
+                        <td>
+                    <?php echo $form['name']->renderError() ?>
+                    <?php echo $form['name'] ?>
+                </td>
+            </tr>
+            <tr>
+                <th><?php echo $form['message']->renderLabel() ?></th>
+                <td>
+                    <?php echo $form['message']->renderError() ?>
+                    <?php echo $form['message'] ?>
+                </td>
+            </tr>
+            <tr>
+                <th><?php echo $form['tags']->renderLabel() ?></th>
+                <td>
+                    <?php include_component('taggableComplete', 'tagWidget', array('object' => $form->getObject())) ?>
+                </td>
+            </tr>
+        </tbody>
+    </table>
 </form>
+
+<?php use_javascript('../sfDoctrineActAsTaggablePlugin/js/pkTagahead'); ?>
+                    <script>
+                        pkTagahead(<?php echo json_encode(url_for("talk/suggest")) ?>);
+</script>
