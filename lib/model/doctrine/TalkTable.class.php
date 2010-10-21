8563a66ca9cc999ac292116a866273e3d5bc304c
Sergey Belov <limitium@users.noreply.github.com>
2010-10-21 18:06:43 +0000

    talks by tag word

--- lib/model/doctrine/TalkTable.class.php
+++ lib/model/doctrine/TalkTable.class.php
@@ -1,6 +1,15 @@
 <?php
 
-class TalkTable extends Doctrine_Table
-{
+class TalkTable extends Doctrine_Table {
+
+    public function getTagQuery() {
+        return $this
+                ->createQuery('t')
+                ->leftJoin('t.CommentTalk')
+                ->leftJoin('t.CreatedBy')
+                ->leftJoin('t.Tagging tg')
+                ->leftJoin('tg.Tag tag')
+                ->orderBy('t.created_at desc');
+    }
 
 }
