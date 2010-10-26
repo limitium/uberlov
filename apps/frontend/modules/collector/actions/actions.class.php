165b221f2124e1ce4b8dce74aeb293a29087123a
limitium <limitium@50e93683-1401-4f79-b91e-9bcc27dc36b6>
2010-10-26 21:18:05 +0000

    changed routes

--- apps/frontend/modules/collector/actions/actions.class.php
+++ apps/frontend/modules/collector/actions/actions.class.php
@@ -9,54 +9,54 @@
  * @version    SVN: $Id: actions.class.php 12479 2008-10-31 10:54:40Z fabien $
  */
 class collectorActions extends sfActions {
-/**
- * Executes map action
- *
- * @param sfRequest $request A request object
- */
-  public function executeMap(sfWebRequest $request) {
-  //echo sfConfig::get('app_map_key');
-  }
 
-  /**
-   * Executes data action
-   *
-   * @param sfRequest $request A request object
-   */
-  public function executeData(sfWebRequest $request) {
-    $this->locations = Doctrine::getTable('Location')
-        ->createQuery('r')
-        ->execute();
-  }
-
-  public function executeImport(sfWebRequest $request) {
-    $form = new ImportForm();
-
-    if($request->isMethod(sfRequest::POST)) {
-      $form->bind(
-          $request->getParameter($form->getName()),
-          $request->getFiles($form->getName())
-      );
-      if ($form->isValid()) {
-        $file = $form->getValue('file');
-        $fileText = file_get_contents($file->getTempName());
-        unlink($file->getTempName());
-        $fileStrings = explode("\r\n", $fileText);
-
-        $OZIdata = explode(' ',$fileStrings[0]);
-        $version = array_pop($OZIdata);
-        $type = ucfirst(strtolower($OZIdata[1]));
-        $OZItypes = array('Waypoint','Track');
-
-        if(in_array($type, $OZItypes)) {
-          $method = 'parse'.$type;
-          $this->locations = OZIConverter::$method($fileStrings,$version);
-          $this->setTemplate('import'.$type);
-        }
-      }
+    /**
+     * Executes map action
+     *
+     * @param sfRequest $request A request object
+     */
+    public function executeLocations(sfWebRequest $request) {
+        //echo sfConfig::get('app_map_key');
     }
-    $this->form = $form;
-  }
 
-  
+    /**
+     * Executes data action
+     *
+     * @param sfRequest $request A request object
+     */
+    public function executeData(sfWebRequest $request) {
+        $this->locations = Doctrine::getTable('Location')
+                        ->createQuery('r')
+                        ->execute();
+    }
+
+    public function executeImport(sfWebRequest $request) {
+        $form = new ImportForm();
+
+        if ($request->isMethod(sfRequest::POST)) {
+            $form->bind(
+                    $request->getParameter($form->getName()),
+                    $request->getFiles($form->getName())
+            );
+            if ($form->isValid()) {
+                $file = $form->getValue('file');
+                $fileText = file_get_contents($file->getTempName());
+                unlink($file->getTempName());
+                $fileStrings = explode("\r\n", $fileText);
+
+                $OZIdata = explode(' ', $fileStrings[0]);
+                $version = array_pop($OZIdata);
+                $type = ucfirst(strtolower($OZIdata[1]));
+                $OZItypes = array('Waypoint', 'Track');
+
+                if (in_array($type, $OZItypes)) {
+                    $method = 'parse' . $type;
+                    $this->locations = OZIConverter::$method($fileStrings, $version);
+                    $this->setTemplate('import' . $type);
+                }
+            }
+        }
+        $this->form = $form;
+    }
+
 }
