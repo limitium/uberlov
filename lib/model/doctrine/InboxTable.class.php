4ca03d82933e94bb38fbb147c1ab9b4a14a3774b
Limitium <Limitium@50e93683-1401-4f79-b91e-9bcc27dc36b6>
2010-11-15 13:18:48 +0000

    menu uppdated

--- lib/model/doctrine/InboxTable.class.php
+++ lib/model/doctrine/InboxTable.class.php
@@ -2,12 +2,4 @@
 
 class InboxTable extends Doctrine_Table {
 
-    public function myInboxCount() {
-        return $this->createQuery('i')
-                ->leftJoin('i.Inboxed id')
-                ->where('i.created_by = ?', sfContext::getInstance()->getUser()->getProfile()->getId())
-                ->andWhere('id.id = ?', sfContext::getInstance()->getUser()->getProfile()->getId())
-                ->count();
-    }
-
 }
