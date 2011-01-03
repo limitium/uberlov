e21c8d4d77f1954602d829635e04d19071ea7994
Sergey Belov <limitium@users.noreply.github.com>
2011-01-03 21:29:18 +0000

    profile form done

--- apps/frontend/modules/profile/templates/_form.php
+++ apps/frontend/modules/profile/templates/_form.php
@@ -1,6 +1,5 @@
 <?php use_stylesheets_for_form($form) ?>
 <?php use_javascripts_for_form($form) ?>
-<?php use_javascript('formShow'); ?>
 <form action="<?php echo url_for('profile/update?id=' . $form->getObject()->getId()) ?>" method="post" <?php $form->isMultipart() and print 'enctype="multipart/form-data" ' ?>>
         <table class="form">
             <tfoot>
