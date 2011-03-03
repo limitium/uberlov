e245837f2b7e582a4ccb2ecb41100070b9d0a7bb
Limitium <Limitium@50e93683-1401-4f79-b91e-9bcc27dc36b6>
2011-03-03 10:21:23 +0000

    events listn

--- apps/frontend/modules/event/templates/listSuccess.php
+++ apps/frontend/modules/event/templates/listSuccess.php
@@ -1,7 +1,18 @@
-<?php
-
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-?>
+<div class="eventList">
+    <h2>Ближайшие события</h2>
+    <?php if ($events->count()): ?>
+        <ul>
+            <?php foreach ($events as $event): ?>
+                <li>
+                    <?php echo $event->getDateTimeObject('date')->format('d.m.Y') ?>
+                    <?php echo link_to($event->getName(), 'event/show?id=' . $event->getId()); ?>
+                    на
+                    <?php echo link_to($event->getLocation()->getName(), 'location/show?id=' . $event->getLocation()->getId()); ?>
+                    <?php //include_partial('profile/addBy', array('added' => $event)); ?>
+                </li>
+            <?php endforeach; ?>
+        </ul>
+    <?php else: ?>
+        Событий нет.
+    <?php endif; ?>
+</div>
