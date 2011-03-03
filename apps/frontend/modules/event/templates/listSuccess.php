536b4aa125c01265103f13d4bc54a0609979f7a3
Limitium <Limitium@50e93683-1401-4f79-b91e-9bcc27dc36b6>
2011-03-03 13:01:08 +0000

    @todo  friends request  event->profile link
    changed events list

--- apps/frontend/modules/event/templates/listSuccess.php
+++ apps/frontend/modules/event/templates/listSuccess.php
@@ -4,11 +4,7 @@
         <ul>
             <?php foreach ($events as $event): ?>
                 <li>
-                    <?php echo $event->getDateTimeObject('date')->format('d.m.Y') ?>
-                    <?php echo link_to($event->getName(), 'event/show?id=' . $event->getId()); ?>
-                    на
-                    <?php echo link_to($event->getLocation()->getName(), 'location/show?id=' . $event->getLocation()->getId()); ?>
-                    <?php //include_partial('profile/addBy', array('added' => $event)); ?>
+                    <?php include_partial('event/list', array('event' => $event)); ?>
                 </li>
             <?php endforeach; ?>
         </ul>
