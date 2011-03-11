bb6c87b96eb4636f0ee15c3bd64eea32c6f7e444
Sergey Belov <limitium@users.noreply.github.com>
2011-03-11 20:34:05 +0000

    added my friends list

--- apps/frontend/modules/profile/templates/friendsSuccess.php
+++ apps/frontend/modules/profile/templates/friendsSuccess.php
@@ -1,4 +1,5 @@
 <?php use_javascript('friendShow'); ?>
+<?php use_stylesheet('friend'); ?>
 <script type="text/javascript">
     app.csrf.friend = "<?php echo $csrf; ?>";
 </script>
@@ -6,9 +7,18 @@
 <div id="fiendsMy">
     <h4>Мои друзья:</h4>
     <?php foreach ($sf_user->getProfile()->getFriends() as $friend): ?>
-        <div><?php echo link_to($friend->getNickName(), 'profile/show?id=' . $friend->getId()); ?>
-            <?php echo link_to('убрать', 'profile/remove?id=' . $friend->getId(), array('class' => 'removeFriend', 'user' => $friend->getId())); ?>        
-            <?php echo link_to('написать инбокс', 'profile/remove?id=' . $friend->getId(), array('class' => 'removeFriend', 'user' => $friend->getId())); ?>        
+        <div>
+            <?php echo link_to(image_tag($friend->getUserpic() ? '/images/userpic/' . $friend->getUserpic() : '/images/userpic/' . ($friend->getSex() ? 'male' : 'female') . '.png'), 'profile/show?id=' . $friend->getId(), array('class' => 'userpic')); ?>
+            <div class="meta">
+                <div>Кто:</div>
+                <?php echo link_to($friend->getFirstName() . ' ' . $friend->getNickName() . ' ' . $friend->getLastName(), 'profile/show?id=' . $friend->getId()); ?>
+                <div>Откуда:</div>
+                <span><?php echo'Обнинск' ?></span>
+            </div>
+            <div class="actions">
+                <?php echo link_to('убрать', 'profile/remove?id=' . $friend->getId(), array('class' => 'removeFriend', 'user' => $friend->getId())); ?>        
+                <?php echo link_to('написать инбокс', 'profile/remove?id=' . $friend->getId(), array('class' => 'removeFriend', 'user' => $friend->getId())); ?>        
+            </div>
         </div>
     <?php endforeach; ?>
 </div>
--- apps/frontend/modules/user/templates/_menu.php
+++ apps/frontend/modules/user/templates/_menu.php
@@ -1,9 +1,9 @@
 <div id="userMenu">
-   <ul>
-       <?php foreach ($menu as $menuItem): ?>
-       <li>
-           <?php echo link_to(html_entity_decode($menuItem['title']), $menuItem['url'])?>
-       </li>
-       <?php endforeach; ?>
-   </ul>
+    <ul>
+        <?php foreach ($menu as $menuItem): ?>
+            <li>
+                <?php echo link_to(html_entity_decode($menuItem['title']), $menuItem['url']) ?>
+            </li>
+        <?php endforeach; ?>
+    </ul>
 </div>
