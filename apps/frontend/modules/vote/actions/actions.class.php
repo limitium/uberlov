1871822ae8d2f7ed96072dcae7074953bb2dc14e
Sergey Belov <limitium@users.noreply.github.com>
2011-03-11 23:41:46 +0000

    changed vote system

--- apps/frontend/modules/vote/actions/actions.class.php
+++ apps/frontend/modules/vote/actions/actions.class.php
@@ -10,90 +10,36 @@
  */
 class voteActions extends sfActions {
 
-    public function executeProfit(sfWebRequest $request) {
-        $this->vote($request, 'profit');
-    }
+    private static $allowed = array('profile', 'comment', 'profit', 'talk', 'fishEvent', 'location');
+    private static $modificators = array('profile' => 'sfGuardUserProfile');
 
-    public function executeTalk(sfWebRequest $request) {
-        $this->vote($request, 'talk');
-    }
+    public function executeFor(sfWebRequest $request) {
+        $type = $request->getParameter('subj');
 
-    public function executeLocation(sfWebRequest $request) {
-        $this->vote($request, 'location');
-    }
+        $this->forward404Unless(in_array($type, self::$allowed));
 
-    public function executeComment(sfWebRequest $request) {
-        $this->vote($request, 'comment');
-    }
-
-    private function vote(sfWebRequest $request, $type) {
-        //@todo csrf check add!!!!
         $uType = ucfirst($type);
+
         $profile = $this->getUser()->getProfile();
-        $object = Doctrine::getTable($uType)->find($request->getParameter('id'));
-
-        $this->forward404Unless($profile);
+        $object = Doctrine::getTable(isset(self::$modificators[$type]) ? self::$modificators[$type] : $uType)->find($request->getParameter('id'));
         $this->forward404Unless($object);
-
+        //@todo: make update instad of delete/add
+        //@todo: check self voting
         Doctrine_Query::create()
-            ->delete('Vote v')
-            ->where('v.voter = ? and '.$type.'_id=?',array($profile->getId(),$object->getId()))
-            ->execute();
+                ->delete('Vote v')
+                ->where('v.voter = ? and ' . $type . '_id=?', array($profile->getId(), $object->getId()))
+                ->execute();
 
-        $voteType  = 'Vote'.$uType;
+        $voteType = 'Vote' . $uType;
         $vl = new $voteType();
         $vl->$uType = $object;
         $vl->Voter = $profile;
-        $vl->value = ($request->getParameter('up')?1:-1) * $profile->getForce();
+        $vl->value = ($request->getParameter('up') ? 1 : -1) * $profile->getForce();
         $vl->save();
 
+        $object->$voteType->add($vl);
         $this->rating = $object->getRating();
         $this->setTemplate('vote');
     }
-    public function executeNew(sfWebRequest $request) {
-        $this->form = new VoteForm();
-    }
 
-    public function executeCreate(sfWebRequest $request) {
-        $this->forward404Unless($request->isMethod(sfRequest::POST));
-
-        $this->form = new VoteForm();
-
-        $this->processForm($request, $this->form);
-
-        $this->setTemplate('new');
-    }
-
-    public function executeEdit(sfWebRequest $request) {
-        $this->forward404Unless($vote = Doctrine::getTable('Vote')->find(array($request->getParameter('id'))), sprintf('Object vote does not exist (%s).', $request->getParameter('id')));
-        $this->form = new VoteForm($vote);
-    }
-
-    public function executeUpdate(sfWebRequest $request) {
-        $this->forward404Unless($request->isMethod(sfRequest::POST) || $request->isMethod(sfRequest::PUT));
-        $this->forward404Unless($vote = Doctrine::getTable('Vote')->find(array($request->getParameter('id'))), sprintf('Object vote does not exist (%s).', $request->getParameter('id')));
-        $this->form = new VoteForm($vote);
-
-        $this->processForm($request, $this->form);
-
-        $this->setTemplate('edit');
-    }
-
-    public function executeDelete(sfWebRequest $request) {
-        $request->checkCSRFProtection();
-
-        $this->forward404Unless($vote = Doctrine::getTable('Vote')->find(array($request->getParameter('id'))), sprintf('Object vote does not exist (%s).', $request->getParameter('id')));
-        $vote->delete();
-
-        $this->redirect('vote/index');
-    }
-
-    protected function processForm(sfWebRequest $request, sfForm $form) {
-        $form->bind($request->getParameter($form->getName()), $request->getFiles($form->getName()));
-        if ($form->isValid()) {
-            $vote = $form->save();
-
-            $this->redirect('vote/edit?id='.$vote->getId());
-        }
-    }
 }
