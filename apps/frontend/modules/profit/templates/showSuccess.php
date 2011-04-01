b346de9990223a003d9e6dddf26933ec9730f423
Sergey Belov <limitium@users.noreply.github.com>
2011-04-01 07:37:32 +0000

    try make edit profit

--- apps/frontend/modules/profit/templates/showSuccess.php
+++ apps/frontend/modules/profit/templates/showSuccess.php
@@ -1,7 +1,12 @@
 <?php include_partial('location/location', array('location' => $profit->getLocation())) ?>
 
 <div class="profitFull">
-    <h2><?php echo $profit->getName(); ?></h2>
+    <h2>
+        <?php echo $profit->getName(); ?>
+        <?php if ($sf_user->getProfile() == $profit->getCreatedBy()): ?>
+            <?php echo link_to(image_tag('/images/ui/edit.png'), 'profit/edit?id=' . $profit->getId()) ?>
+        <?php endif; ?>
+    </h2>
     <div class="where">Ловили <?php echo $profit->getDateTimeObject('date')->format('d.m.Y'); ?> на <?php echo link_to($profit->getLocation()->getName(), 'location/show?id=' . $profit->getLocation()->getId()); ?></div>
     <table class="profitDetails">
         <thead>
@@ -15,7 +20,7 @@
             <?php $total = 0;
             $i = 0; ?>
             <?php foreach ($profit->getProfitDetail() as $det): ?>
-            <?php $total += $det->qty;
+                <?php $total += $det->qty;
                 $i++; ?>
                 <tr class="<?php echo fmod($i, 2) ? 'even' : 'odd' ?>">
                     <td><?php echo $det->getStyle(); ?></td>
@@ -23,40 +28,40 @@
                     <td><?php echo $det->qty; ?></td>
                 </tr>
             <?php endforeach; ?>
-            </tbody>
-            <tfoot>
-                <tr>
-                    <td></td>
-                    <td></td>
-                    <td><?php echo $total; ?></td>
-                </tr>
-            </tfoot>
-        </table>
-        <div class="best">
-            Самая большая:
+        </tbody>
+        <tfoot>
+            <tr>
+                <td></td>
+                <td></td>
+                <td><?php echo $total; ?></td>
+            </tr>
+        </tfoot>
+    </table>
+    <div class="best">
+        Самая большая:
         <?php if ($profit->fish_id): ?>
-        <?php echo $profit->getFish(); ?> на <?php echo $profit->getWeight(); ?> кг.
+            <?php echo $profit->getFish(); ?> на <?php echo $profit->getWeight(); ?> кг.
         <?php else: ?>
-                        нету :(
+            нету :(
         <?php endif; ?>
-                    </div>
-                    <div>Использовались снасти: <?php echo $profit->getCordage(); ?></div>
-                    <div class="text"><?php echo $profit->getDescription(); ?></div>
-                    <div class="meta">
+    </div>
+    <div>Использовались снасти: <?php echo $profit->getCordage(); ?></div>
+    <div class="text"><?php echo $profit->getDescription(); ?></div>
+    <div class="meta">
         <?php use_javascript('voting'); ?>
         <?php include_partial('vote/vote', array('obj' => $profit)); ?>
-                        <div>
-                            <a href="" id="goToReply">□</a> <?php include_partial('profile/writeBy', array('written' => $profit)); ?> |
-                            <a href="" class="commentShowAuthor" author="user<?php echo $profit->getCreatedBy(); ?>">●</a>
-                        </div>
-                    </div>
-                </div>
+        <div>
+            <a href="" id="goToReply">□</a> <?php include_partial('profile/writeBy', array('written' => $profit)); ?> |
+            <a href="" class="commentShowAuthor" author="user<?php echo $profit->getCreatedBy(); ?>">●</a>
+        </div>
+    </div>
+</div>
 
-                <div class="tabPanel">
-                    <ul>
-                        <li><span href="#" id="tabComments" class="selected">Комментарии (<i id="commentCounter"><?php echo sizeof($comments); ?></i>)</span></li>
-                    </ul>
-                </div>
+<div class="tabPanel">
+    <ul>
+        <li><span href="#" id="tabComments" class="selected">Комментарии (<i id="commentCounter"><?php echo sizeof($comments); ?></i>)</span></li>
+    </ul>
+</div>
 
 <?php include_partial('comment/tree', array('form' => $form, 'comments' => $comments)); ?>
 
