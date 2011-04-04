06db3b94756124e8ea498c4a4d35c80f10aa7348
Sergey Belov <limitium@users.noreply.github.com>
2011-04-04 08:47:49 +0000

    talk section herarchy fixde

--- apps/frontend/modules/talk/templates/_section.php
+++ apps/frontend/modules/talk/templates/_section.php
@@ -1,14 +1,11 @@
 <div class="sectionMenu">
     <h4>Раздел:</h4>
     <?php echo link_to('Все', 'talk/list'); ?>
-    <?php
-    $tree = Doctrine_Core::getTable('TalkSection')
-                    ->createQuery('c')
-                    ->execute(); ?>
+    <?php $tree = Doctrine_Core::getTable('TalkSection')->getTree()->fetchTree(); ?>
     <?php $tree instanceof Doctrine_Tree_NestedSet; ?>
 
     <?php foreach ($tree->toHierarchy() as $section) : ?>
-    <?php include_partial('talk/section_node', array('node' => $section, 'curSection' => $curSection)); ?>
+        <?php include_partial('talk/section_node', array('node' => $section, 'curSection' => $curSection)); ?>
     <?php endforeach; ?>
     <?php echo link_to('Добавить', 'talk/section' . ($curSection ? '?parent=' . $curSection->getId() : '')); ?>
 </div>
