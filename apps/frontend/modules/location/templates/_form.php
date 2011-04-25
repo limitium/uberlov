189bd77a2c1c4f0b16e14d57074719a212b53df4
limitium <limitium@50e93683-1401-4f79-b91e-9bcc27dc36b6>
2011-04-25 22:17:11 +0000

    map css

--- apps/frontend/modules/location/templates/_form.php
+++ apps/frontend/modules/location/templates/_form.php
@@ -1,90 +1,99 @@
 <?php use_stylesheets_for_form($form) ?>
 <?php use_javascripts_for_form($form) ?>
-<form class ="location form" action="<?php echo url_for('location/'.($form->getObject()->isNew() ? 'create' : 'update').(!$form->getObject()->isNew() ? '?id='.$form->getObject()->getId() : '')) ?>" method="post" <?php $form->isMultipart() and print 'enctype="multipart/form-data" ' ?>>
-    <?php if (!$form->getObject()->isNew()): ?>
-    <input type="hidden" name="sf_method" value="put" />
-    <?php endif; ?>
-    <table>
-        <tfoot>
-            <tr>
-                <td colspan="2">
-                    <?php echo $form->renderHiddenFields() ?>
-                    <input type="submit" class="button" value="<?php echo $form->getObject()->isNew() ? 'Добавить' : 'Сохранить'; ?>" />
-                </td>
-            </tr>
-        </tfoot>
-        <tbody>
-            <?php echo $form->renderGlobalErrors() ?>
-            <tr>
-                <th><?php echo $form['name']->renderLabel() ?></th>
-                <td>
-                    <?php echo $form['name']->renderError() ?>
-                    <?php echo $form['name'] ?>
-                </td>
-            </tr>
-            <tr>
-                <th><?php echo $form['location_type_id']->renderLabel() ?></th>
-                <td>
-                    <?php echo $form['location_type_id']->renderError() ?>
-                    <?php echo $form['location_type_id'] ?>
-                </td>
-            </tr>
-            <tr>
-                <th><?php echo $form['depth']->renderLabel() ?></th>
-                <td>
-                    <?php echo $form['depth']->renderError() ?>
-                    <?php echo $form['depth'] ?>
-                </td>
-            </tr>
-            <tr>
-                <th><?php echo $form['location_flow_id']->renderLabel() ?></th>
-                <td>
+
+<?php if ($form->hasGlobalErrors()): ?>
+    <ul class="error_list">
+        <?php foreach ($form->getGlobalErrors() as $name => $error): ?>
+            <li><?php echo $name . ': ' . $error ?></li>
+        <?php endforeach; ?>
+    </ul>
+<?php endif; ?>
+
+<form class ="location form" action="<?php echo url_for('location/' . ($form->getObject()->isNew() ? 'create' : 'update') . (!$form->getObject()->isNew() ? '?id=' . $form->getObject()->getId() : '')) ?>" method="post" <?php $form->isMultipart() and print 'enctype="multipart/form-data" ' ?>>
+    <fieldset>
+        <dl>
+            <dt><?php echo $form['name']->renderLabel() ?></dt>
+            <dd>
+                <?php echo $form['name']->renderError() ?>
+                <?php echo $form['name'] ?>
+            </dd>
+        </dl>
+        <dl>
+            <dt><?php echo $form['location_type_id']->renderLabel() ?></dt>
+            <dd>
+                <?php echo $form['location_type_id']->renderError() ?>
+                <?php echo $form['location_type_id'] ?>
+            </dd>
+        </dl>
+        <dl>
+            <dt><?php echo $form['depth']->renderLabel() ?></dt>
+            <dd>
+                <?php echo $form['depth']->renderError() ?>
+                <?php echo $form['depth'] ?>
+            </dd>
+            <dl>
+                <dt><?php echo $form['location_flow_id']->renderLabel() ?></dt>
+                <dd>
                     <?php echo $form['location_flow_id']->renderError() ?>
                     <?php echo $form['location_flow_id'] ?>
-                </td>
-            </tr>
-            <tr>
-                <th><?php echo $form['location_fundus_id']->renderLabel() ?></th>
-                <td>
+                </dd>
+            </dl>
+            <dl>
+                <dt><?php echo $form['location_fundus_id']->renderLabel() ?></dt>
+                <dd>
                     <?php echo $form['location_fundus_id']->renderError() ?>
                     <?php echo $form['location_fundus_id'] ?>
-                </td>
-            </tr>
-            <tr>
-                <th><?php echo $form['location_relief_id']->renderLabel() ?></th>
-                <td>
+                </dd>
+            </dl>
+            <dl>
+                <dt><?php echo $form['location_relief_id']->renderLabel() ?></dt>
+                <dd>
                     <?php echo $form['location_relief_id']->renderError() ?>
                     <?php echo $form['location_relief_id'] ?>
-                </td>
-            </tr>
-            <tr>
-                <th><?php echo $form['location_scope_id']->renderLabel() ?></th>
-                <td>
-                    <?php echo $form['location_scope_id']->renderError() ?>
-                    <?php echo $form['location_scope_id'] ?>
-                </td>
-            </tr>
-            <tr>
-                <th><?php echo $form['is_free']->renderLabel() ?></th>
-                <td>
-                    <?php echo $form['is_free']->renderError() ?>
-                    <?php echo $form['is_free'] ?>
-                </td>
-            </tr>
-            <tr>
-                <th><?php echo $form['price']->renderLabel() ?></th>
-                <td>
-                    <?php echo $form['price']->renderError() ?>
-                    <?php echo $form['price'] ?>
-                </td>
-            </tr>  
-            <tr>
-                <th><?php echo $form['description']->renderLabel() ?></th>
-                <td>
-                    <?php echo $form['description']->renderError() ?>
-                    <?php echo $form['description'] ?>
-                </td>
-            </tr>
-        </tbody>
-    </table>    
+                </dd>
+            </dl>
+    </fieldset>
+    <fieldset>
+        <dl>
+            <dt><?php echo $form['location_scope_id']->renderLabel() ?></dt>
+            <dd>
+                <?php echo $form['location_scope_id']->renderError() ?>
+                <?php echo $form['location_scope_id'] ?>
+            </dd>
+        </dl>
+        <dl>
+            <dt><?php echo $form['is_free']->renderLabel() ?></dt>
+            <dd>
+                <?php echo $form['is_free']->renderError() ?>
+                <?php echo $form['is_free'] ?>
+            </dd>
+        </dl>
+        <dl>
+            <dt><?php echo $form['price']->renderLabel() ?></dt>
+            <dd>
+                <?php echo $form['price']->renderError() ?>
+                <?php echo $form['price'] ?>
+            </dd>
+        </dl>
+        <dl>
+            <dt><?php echo $form['description']->renderLabel() ?></dt>
+            <dd>
+                <?php echo $form['description']->renderError() ?>
+                <?php echo $form['description'] ?>
+            </dd>
+        </dl>
+    </fieldset>
+    <div class="form_footer">
+        <?php if (!$form->getObject()->isNew()): ?>
+            <input type="hidden" name="sf_medtod" value="put" />
+        <?php endif; ?>
+        <?php echo $form->renderHiddenFields(false) ?>
+        <button class="button_01" type="submit">
+            <span class="border_l">
+                <span class="border_r">
+                    <span class="btn_bg"><?php echo $form->getObject()->isNew() ? 'ДОБАВИТЬ' : 'СОХРАНИТЬ'; ?></span>
+                </span>
+            </span>
+        </button>
+    </div>
 </form>
---
+++ lib/filter/doctrine/base/BaseFishEventTypeFormFilter.class.php
@@ -0,0 +1,47 @@
+<?php
+
+/**
+ * FishEventType filter form base class.
+ *
+ * @package    FISHERY
+ * @subpackage filter
+ * @author     Sergei Belov <limitium@gmail.com>
+ * @version    SVN: $Id: sfDoctrineFormFilterGeneratedTemplate.php 29570 2010-05-21 14:49:47Z Kris.Wallsmith $
+ */
+abstract class BaseFishEventTypeFormFilter extends BaseFormFilterDoctrine
+{
+  public function setup()
+  {
+    $this->setWidgets(array(
+      'name'   => new sfWidgetFormFilterInput(array('with_empty' => false)),
+      'weight' => new sfWidgetFormFilterInput(),
+    ));
+
+    $this->setValidators(array(
+      'name'   => new sfValidatorPass(array('required' => false)),
+      'weight' => new sfValidatorSchemaFilter('text', new sfValidatorInteger(array('required' => false))),
+    ));
+
+    $this->widgetSchema->setNameFormat('fish_event_type_filters[%s]');
+
+    $this->errorSchema = new sfValidatorErrorSchema($this->validatorSchema);
+
+    $this->setupInheritance();
+
+    parent::setup();
+  }
+
+  public function getModelName()
+  {
+    return 'FishEventType';
+  }
+
+  public function getFields()
+  {
+    return array(
+      'id'     => 'Number',
+      'name'   => 'Text',
+      'weight' => 'Number',
+    );
+  }
+}
