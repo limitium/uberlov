d69b49c55a896dfbcbadea5f40f326626958d583
Limitium <Limitium@50e93683-1401-4f79-b91e-9bcc27dc36b6>
2011-05-10 07:53:22 +0000

    regions

--- apps/frontend/modules/location/templates/_location_list.php
+++ apps/frontend/modules/location/templates/_location_list.php
@@ -2,9 +2,9 @@
     <ul>
         <?php foreach ($locations as $location): ?>
             <li>
-            <?php echo link_to($location->getName(), 'location/show?id=' . $location->getId()); ?>
-            <?php include_partial('profile/addBy', array('added' => $location)); ?>
-        </li>
+                <h4><?php echo link_to($location->getName(), 'location/show?id=' . $location->getId()); ?></h4>                
+                <div class="meta"><?php include_partial('profile/addBy', array('added' => $location)); ?></div>
+            </li>
         <?php endforeach; ?>
     </ul>
 </div>
