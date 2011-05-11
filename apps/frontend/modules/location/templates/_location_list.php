6f24ac59eeb929638a06cc520ece834ef368aff3
Sergey Belov <limitium@users.noreply.github.com>
2011-05-11 07:09:11 +0000

    qweqwe

--- apps/frontend/modules/location/templates/_location_list.php
+++ apps/frontend/modules/location/templates/_location_list.php
@@ -1,9 +1,14 @@
+<?php use_helper('XssSafe'); ?>
 <div class="locationList">
     <ul>
         <?php foreach ($locations as $location): ?>
             <li>
                 <h4><?php echo link_to($location->getName(), 'location/show?id=' . $location->getId()); ?></h4>                
-                <div class="meta"><?php include_partial('profile/addBy', array('added' => $location)); ?></div>
+                <div class="meta">
+                    <?php include_partial('vote/vote', array('obj' => $location)); ?>
+                    <?php include_partial('profile/addBy', array('added' => $location)); ?>
+                    <?php include_partial('comment/meta', array('commented' => $location)); ?>
+                </div>
             </li>
         <?php endforeach; ?>
     </ul>
