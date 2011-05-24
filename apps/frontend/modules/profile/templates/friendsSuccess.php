4bd45f643ebb8326a54231fecf67d092e01928db
Sergey Belov <limitium@users.noreply.github.com>
2011-05-24 08:34:25 +0000

    profile frinds

--- apps/frontend/modules/profile/templates/friendsSuccess.php
+++ apps/frontend/modules/profile/templates/friendsSuccess.php
@@ -3,26 +3,31 @@
 <script type="text/javascript">
     app.csrf.friend = "<?php echo $csrf; ?>";
 </script>
+
 <div id="fiendsMy">
-    <?php if($friends->count()): ?>
-    <h2>Мои друзья:</h2>
-    <?php foreach ($firends as $friend): ?>
-        <div>
-            <?php echo link_to(image_tag($friend->getUserpic() ? '/images/userpic/' . $friend->getUserpic() : '/images/userpic/' . ($friend->getSex() ? 'male' : 'female') . '.png'), 'profile/show?id=' . $friend->getId(), array('class' => 'userpic')); ?>
-            <div class="meta">
-                <div>Кто:</div>
-                <?php echo link_to($friend->getFirstName() . ' ' . $friend->getNickName() . ' ' . $friend->getLastName(), 'profile/show?id=' . $friend->getId()); ?>
-                <div>Откуда:</div>
-                <span><?php echo'Обнинск' ?></span>
-            </div>
-            <div class="actions">
-                <?php echo link_to('убрать', 'profile/remove?id=' . $friend->getId(), array('class' => 'removeFriend', 'user' => $friend->getId())); ?>        
-                <?php echo link_to('написать инбокс', 'profile/remove?id=' . $friend->getId(), array('class' => 'removeFriend', 'user' => $friend->getId())); ?>        
-            </div>
-        </div>
-    <?php endforeach; ?>
-    <?php else: ?>
-    <?php include_partial('friends', array('profile' => $profile, 'csrf' => $csrf)); ?>
-    <h2>Тут вы еще одиноки.</h2>
-    <?php endif; ?>
+    <div class="requesters<?php echo $profile->getRequesters()->count() ? '' : ' empty'; ?>">
+        <h2>Хотят дружить:</h2>
+        <ul>
+            <?php foreach ($profile->getRequesters() as $friend): ?>
+                <li>
+                    <?php include_partial('profile/listItem', array('profile' => $friend)); ?>
+                </li>
+            <?php endforeach; ?>
+        </ul>
+    </div>
+
+
+
+    <div class="accepted<?php echo $profile->getFriends()->count() > 0 ? '' : ' empty'; ?>">
+        <h2>Мои друзья:</h2>
+        <ul>
+            <?php foreach ($profile->getFriends() as $friend): ?>
+                <li><?php include_partial('profile/picnick', array('profile' => $friend)); ?>
+                    <?php if (!$sf_user->isAnonymous() && $profile == $sf_user->getProfile()): ?>
+                        <?php echo link_to('-', 'profile/remove?id=' . $friend->getId(), array('class' => 'removeFriend', 'user' => $friend->getId(), 'title' => 'Удалить из друзей')); ?>        
+                    <?php endif; ?>
+                </li>
+            <?php endforeach; ?>
+        </ul>
+    </div>
 </div>
