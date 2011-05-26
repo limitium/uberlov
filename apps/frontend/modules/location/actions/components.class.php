19675a543be56496c33a8731f670e3e8bcab6504
limitium <limitium@50e93683-1401-4f79-b91e-9bcc27dc36b6>
2011-05-26 21:28:11 +0000

    fixed user bluk load

--- apps/frontend/modules/location/actions/components.class.php
+++ apps/frontend/modules/location/actions/components.class.php
@@ -12,8 +12,10 @@ class locationComponents extends sfComponents {
 
 
     public function executeLast() {
-        $this->locations = Doctrine_Query::create()->select()->from('Location l')
+        $this->locations = Doctrine::getTable('Location')
+                ->createQuery('l')
                 ->leftJoin('l.CreatedBy p')
+                ->leftJoin('p.User u')
                 ->orderBy('l.created_at desc')
                 ->limit(5)
                 ->execute();
