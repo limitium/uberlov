39ec1e1ffc6cd0f9d826ec653e727e8d074a0964
limitium <limitium@50e93683-1401-4f79-b91e-9bcc27dc36b6>
2011-06-06 21:41:34 +0000

    icons changed

--- apps/frontend/modules/collector/templates/dataSuccess.php
+++ apps/frontend/modules/collector/templates/dataSuccess.php
@@ -1,10 +1,13 @@
 {"locations":[
-<?php $nb = count($locations); $i = 0; foreach ($locations as $location): ++$i ?>
+<?php $nb = count($locations);
+$i = 0;
+foreach ($locations as $location):++$i ?>
     {
     "id": "<?php echo $location->getId() ?>",
     "name": "<?php echo $location->getName() ?>",
     "lat": "<?php echo $location->getLatitude() ?>",
     "lng": "<?php echo $location->getLongitude() ?>",
+    "free": "<?php echo $location->getIsFree() ?>",
     "event": "<?php echo $location->getFishEvent()->count() > 0 ?>"
     }<?php echo $nb == $i ? '' : ',' ?>
 <?php endforeach; ?>
