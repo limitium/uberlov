e580d17967e9568e72e3e7bf0930aab67e6cac28
Sergey Belov <limitium@users.noreply.github.com>
2011-06-06 19:32:36 +0000

    inbx addd fixed

--- apps/frontend/modules/inbox/templates/addSuccess.php
+++ apps/frontend/modules/inbox/templates/addSuccess.php
@@ -2,7 +2,7 @@
 <?php $nb = count($added); $i = 0; foreach ($added as $profile): ++$i ?>
 {
 "id": "<?php echo $profile->getId() ?>",
-"name": "<?php echo $profile->getNickName() ?>",
+"name": "<?php echo $profile->getNickName() ?>"
 }<?php echo $nb == $i ? '' : ',' ?>
 <?php endforeach; ?>
 ]}
