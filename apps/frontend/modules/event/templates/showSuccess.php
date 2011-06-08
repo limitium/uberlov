28e5b8e8afd504459be56d742a24d69e6bed5078
Limitium <Limitium@50e93683-1401-4f79-b91e-9bcc27dc36b6>
2011-06-08 08:10:05 +0000

    event show fixed +  ilike

--- apps/frontend/modules/event/templates/showSuccess.php
+++ apps/frontend/modules/event/templates/showSuccess.php
@@ -3,43 +3,45 @@
 
 
 <?php include_partial('location/location', array('location' => $event->getLocation())) ?>
+<?php include_partial('default/ilike', array('url' => '@event_show?id='.$event->getId())) ?>
 <div class="eventFull">
     <h2><?php echo $event->getDateTimeObject('date')->format('d.m.Y') ?> <?php echo $event->getName(); ?>
         <?php if ($sf_user->getProfile() == $event->getCreatedBy()): ?>
             <?php echo link_to(image_tag('/images/ui/edit.png'), 'event/edit?id=' . $event->getId()) ?>
         <?php endif; ?>
     </h2>
-    <div id="desctiption">
+    <div class="where">Пройдет на <?php echo link_to($event->getLocation()->getName(), 'location/show?id=' . $event->getLocation()->getId()); ?></div>
+    <div class="desctiption">
         <h2>Описание:</h2>
         <div class="text">
             <?php echo $event->getDescription(ESC_XSSSAFE); ?>
         </div>
     </div>
-    <div id="contacts">
+    <div class="contacts">
         <h2>Контакты:</h2>
         <div class="text">
             <?php echo $event->getContact(ESC_XSSSAFE); ?>
         </div>
     </div>
-    <div id="price">
+    <div class="price">
         <h2>Цены:</h2>
         <div class="text">
             <?php echo $event->getPrice(ESC_XSSSAFE); ?>
         </div>
     </div>
-    <div id="schedule">
+    <div class="schedule">
         <h2>Расписание:</h2>
         <div class="text">
             <?php echo $event->getSchedule(ESC_XSSSAFE); ?>
         </div>
     </div>
-    <div id="rules">
+    <div class="rules">
         <h2>Регламент:</h2>
         <div class="text">
             <?php echo $event->getRules(ESC_XSSSAFE); ?>
         </div>
     </div>
-    <div id="users">
+    <div class="users">
         <h2>Участники:</h2>
         <div class="text">
             <?php echo $event->getUsers(ESC_XSSSAFE); ?>
