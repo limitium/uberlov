65d19c766dc4219f8f0c776359097494dee7f6ca
Limitium <Limitium@50e93683-1401-4f79-b91e-9bcc27dc36b6>
2011-09-08 11:09:29 +0000

    descradded

--- apps/frontend/modules/location/templates/searchSuccess.php
+++ apps/frontend/modules/location/templates/searchSuccess.php
@@ -1,7 +1,38 @@
+<?php use_helper('XssSafe'); ?>
 <h2>Поиск мест</h2>
 <?php include_partial('search', array('form' => $form)) ?>
 <?php if (isset($locations)): ?>
-    <?php include_partial('location/location_list', array('locations' => $locations)); ?>
+
+    <div class="locationList">
+        <ul>
+            <?php foreach ($locations as $location): ?>
+                <li>
+                    <h3><?php echo link_to($location->getName(), 'location/show?id=' . $location->getId()); ?></h3>     
+                    <div class="text">
+                        <?php echo $location->getDescription(ESC_XSSSAFE) ?>
+                    </div>
+                    <div class="fish">
+                        <?php $fishes = $location->getFishes(); ?>
+                        <?php if ($fishes->count()): ?>
+                            <ul>
+                                <?php foreach ($fishes as $fish): ?>
+                                    <li><?php echo $fish->name; ?></li>
+                                <?php endforeach; ?>
+                            </ul>
+                        <?php else: ?>
+                            <p>Тут еще не ловили</p>
+                        <?php endif; ?>
+                    </div>
+                    <div class="meta">
+                        <?php include_partial('vote/vote', array('obj' => $location)); ?>
+                        <?php include_partial('profile/addBy', array('added' => $location)); ?>
+                        <?php include_partial('comment/meta', array('commented' => $location)); ?>
+                    </div>
+                </li>
+            <?php endforeach; ?>
+        </ul>
+    </div>
+
     <?php if (!$locations->count()): ?>
         <div class="search_empty">
             Упс. А ничего не найдено...
