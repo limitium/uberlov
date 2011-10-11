959e611c3ceac3dae1a6ad1e815b7fb755751bbd
limitium <limitium@50e93683-1401-4f79-b91e-9bcc27dc36b6>
2011-10-11 21:48:43 +0000

    vote fixed and message

--- apps/frontend/modules/vote/templates/_vote.php
+++ apps/frontend/modules/vote/templates/_vote.php
@@ -1,10 +1,12 @@
 <?php $rating = $obj->getRating(); ?>
 <div class="rating" to="<?php echo isset($objType) ? $objType : $obj->getTable()->getComponentName(); ?>" id="rate<?php echo $obj->getId(); ?>">
-    <a class ="plus<?php echo $obj->plused ? ' voted' : ''; ?>" href="">
-        <?php echo image_tag("/images/plus.png"); ?>
-    </a>
-    <a class="minus<?php echo $obj->minused ? ' voted' : ''; ?>" href="">
-        <?php echo image_tag("/images/minus.png"); ?>
-    </a>
+    <?php if (!$sf_user->isAnonymous()): ?>
+        <a class ="plus<?php echo $obj->plused ? ' voted' : ''; ?>" href="">
+            <?php echo image_tag("/images/plus.png"); ?>
+        </a>
+        <a class="minus<?php echo $obj->minused ? ' voted' : ''; ?>" href="">
+            <?php echo image_tag("/images/minus.png"); ?>
+        </a>
+    <?php endif; ?>
     <span>[<?php echo $rating; ?>]</span>
 </div>
