22cc397c42168f7bb0025b58dbdd7a1194be6dd6
Sergey Belov <limitium@users.noreply.github.com>
2011-10-13 19:23:07 +0000

    nice registration mail

---
+++ apps/frontend/modules/sfApply/templates/_mailTemplate.php
@@ -0,0 +1,107 @@
+<table width="100%" cellspacing="0" cellpadding="0" bgcolor="#EFEFEF">
+    <tbody>
+        <tr>
+            <td width="5%">
+                <table width="15" cellspacing="0" cellpadding="0">
+                    <tbody>
+                        <tr>
+                            <td>
+                                <div style="height:0;line-height:0;font-size:0">&nbsp;</div>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </td>
+            <td width="90%" style="padding:15px 0;">
+                <table width="480" cellspacing="0" cellpadding="0">
+                    <tbody>
+                        <tr>
+                            <td>
+                                <div style="height:0;line-height:0;font-size:0">&nbsp;</div>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+                <table width="100%" cellspacing="0" cellpadding="0" bgcolor="#FFFFFF">
+                    <tbody>
+                        <tr>
+                            <td>
+                                <table width="100%" cellspacing="0" cellpadding="0" bgcolor="#313131">
+                                    <tbody>
+                                        <tr>
+                                            <td width="134" valign="top">
+                                                <a style="color:#FFFFFF;font-size:14px;font-weight:bold;height:68px;line-height:68px;max-height:68px;text-decoration:none;" href="http://uberlov.ru" target="blank">
+                                                    <img width="134" height="68" border="0" style="border:none;display:block;" src="http://uberlov.ru/images/common/backgrounds/mail_headl.gif" alt="&nbsp;&nbsp;Uberlov"></a>
+                                            </td>
+                                            <td width="100%" valign="top" background="http://uberlov.ru/images/common/backgrounds/mail_headb.gif">&nbsp;</td>
+                                            <td width="5" valign="top" bgcolor="#313131" background="http://uberlov.ru/images/common/backgrounds/mail_headb.gif" align="right">
+                                                <img width="5" height="68" border="0" style="border:0;display:block;" src="http://uberlov.ru/images/common/backgrounds/mail_headr.gif">
+                                            </td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <div style="padding:8px 18px 13px 18px;border-left:1px solid #DFDFDF;border-right:1px solid #DFDFDF;font-size:11px;color:black;">
+                                    <h1 style="margin:2px 0px 15px 0;padding:0px 0px 4px;border-bottom:1px solid #DFDFDF;color:#759E1A;font-size:140%;"><?php echo $title; ?></h1>
+                                    <table cellspacing="0" cellpadding="0" border="0">
+                                        <tbody>
+                                            <tr>
+                                                <td style="font-size:100%;"><?php echo $message; ?></td>
+                                            </tr>
+                                        </tbody>
+                                    </table>                                   
+                                </div>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <table width="100%" cellspacing="0" cellpadding="0">
+                                    <tbody>
+                                        <tr>
+                                            <td width="5">
+                                                <div style="width:0;height:1px;max-height:1px;line-height:1px;font-size:0;border-left:1px solid #dfdfdf;border-right:1px solid #ffffff;"></div>
+                                                <div style="width:0;height:1px;max-height:1px;line-height:1px;font-size:0;border-left:1px solid #dfdfdf;border-right:1px solid #ffffff;"></div>
+                                                <div style="width:1px;height:1px;max-height:1px;line-height:1px;font-size:0;border-left:1px solid #EFEFEF;background-color:#dfdfdf;border-right:1px solid #ffffff;"></div>
+                                                <div style="width:1px;height:1px;max-height:1px;line-height:1px;font-size:0;border-left:2px solid #EFEFEF;background-color:#dfdfdf;border-right:1px solid #ffffff;"></div>
+                                                <div style="width:1px;height:1px;max-height:1px;line-height:1px;font-size:0;border-left:3px solid #EFEFEF;background-color:#dfdfdf;border-right:1px solid #dfdfdf;"></div>
+                                            </td>
+                                            <td width="100%" valign="bottom">
+                                                <div style="height:4px;max-height:4px;line-height:0;font-size:0;border-bottom:1px solid #DFDFDF;">&nbsp;</div>
+                                            </td>
+                                            <td width="5" align="right">
+                                                <div style="width:0;height:1px;max-height:1px;line-height:1px;font-size:0;border-left:1px solid #ffffff;border-right:1px solid #dfdfdf;"></div>
+                                                <div style="width:0;height:1px;max-height:1px;line-height:1px;font-size:0;border-left:1px solid #ffffff;border-right:1px solid #EFEFEF;"></div>
+                                                <div style="width:1px;height:1px;max-height:1px;line-height:1px;font-size:0;border-left:1px solid #ffffff;background-color:#dfdfdf;border-right:1px solid #EFEFEF;"></div>
+                                                <div style="width:1px;height:1px;max-height:1px;line-height:1px;font-size:0;border-left:1px solid #ffffff;background-color:#dfdfdf;border-right:2px solid #EFEFEF;"></div>
+                                                <div style="width:1px;height:1px;max-height:1px;line-height:1px;font-size:0;border-left:1px solid #dfdfdf;background-color:#dfdfdf;border-right:3px solid #EFEFEF;"></div>
+                                            </td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td bgcolor="#EFEFEF" align="center" style="padding:13px 0 0 0;font-size:11px;color:black;">
+                                С наилучшими пожеланиями, <a style="color:#0088CC;text-decoration:none;" href="http://uberlov.ru" target="blank">Uberlov.ru</a>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </td>
+            <td width="5%">
+                <table width="15" cellspacing="0" cellpadding="0">
+                    <tbody>
+                        <tr>
+                            <td>
+                                <div style="height:0;line-height:0;font-size:0">&nbsp;</div>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </td>
+        </tr>
+    </tbody>
+</table>
