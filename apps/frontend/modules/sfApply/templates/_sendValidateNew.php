5faba55b7579b2166bf120768b4e7a872c62df00
limitium <limitium@50e93683-1401-4f79-b91e-9bcc27dc36b6>
2011-10-13 19:23:07 +0000

    nice registration mail

--- apps/frontend/modules/sfApply/templates/_sendValidateNew.php
+++ apps/frontend/modules/sfApply/templates/_sendValidateNew.php
@@ -1,5 +1,5 @@
 <?php use_helper('I18N', 'Url') ?>
-<?php echo __('<p>
+<?php include_partial('sfApply/mailTemplate',array("title"=>"Подтверждение регистрации",'message'=>__('<p>
 Thanks for applying for an account on %1%.
 </p><p>
 To prevent abuse of the site, we require that you activate your account by clicking on the following link:
@@ -10,4 +10,4 @@ Thanks again for joining us.
 </p>'
 , array("%1%" => link_to($sf_request->getHost(), $sf_request->getUriPrefix()),
   "%2%" => link_to(url_for("sfApply/confirm?validate=$validate", true), "sfApply/confirm?validate=$validate", array("absolute" => true))),
-        'sfForkedApply') ?>
+        'sfForkedApply')))?>
