cf78ad4e22af4aab70232f29a38f858dad66966f
Sergey Belov <limitium@users.noreply.github.com>
2011-10-25 21:38:19 +0000

    fixed friends & nice tags

--- plugins/sfDoctrineActAsTaggablePlugin/modules/taggableComplete/templates/completeSuccess.php
+++ plugins/sfDoctrineActAsTaggablePlugin/modules/taggableComplete/templates/completeSuccess.php
@@ -1,6 +1,15 @@
 <ul>
-<?php $n = 1; $limit = count($tagSuggestions); foreach($tagSuggestions as $suggestion): ?>
-<?php // No extraneous whitespace here, it shows up in text() ?>
-<li class="<?php echo ($n%2==0?'even':'odd');?>"><span class="tag-spacer left"><?php echo $suggestion['left']?></span><a <?php echo ($n == $limit)? 'class="last"':'' ?><?php echo ($n == 1)? 'class="first"':'' ?> href="#"><?php echo $suggestion['suggested']?></a><span class="tag-spacer left"><?php echo $suggestion['right']?></span></li>
-<?php $n++; endforeach ?>
+    <?php
+    $n = 1;
+    $limit = count($tagSuggestions);
+    foreach ($tagSuggestions as $suggestion):
+        ?>
+        <?php // No extraneous whitespace here, it shows up in text()  ?>
+        <li class="<?php echo ($n % 2 == 0 ? 'even' : 'odd'); ?>">
+            <span class="tag-spacer left"><?php echo $suggestion['left'] ?></span>
+            <a <?php echo ($n == $limit) ? 'class="last"' : '' ?><?php echo ($n == 1) ? 'class="first"' : '' ?> href="#"><?php echo $suggestion['suggested'] ?></a>
+            <span class="tag-spacer right"><?php echo $suggestion['right'] ?></span>
+        </li>
+        <?php $n++;
+    endforeach ?>
 </ul>
