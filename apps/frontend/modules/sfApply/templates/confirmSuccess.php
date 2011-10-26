ad258c5f1e862ce82b10f3cb3b17f7f85ed23e46
Limitium <Limitium@50e93683-1401-4f79-b91e-9bcc27dc36b6>
2011-10-26 10:44:55 +0000

    registration log to analytics

--- apps/frontend/modules/sfApply/templates/confirmSuccess.php
+++ apps/frontend/modules/sfApply/templates/confirmSuccess.php
@@ -4,3 +4,20 @@
 <p>
 Не забудте заполнить свой профаил - <?php echo link_to($sf_user->getUserName(),"@my_edit"); ?>.</p>
 </div>
+<!-- Google Code for register Conversion Page -->
+<script type="text/javascript">
+/* <![CDATA[ */
+var google_conversion_id = 1029164716;
+var google_conversion_language = "en";
+var google_conversion_format = "3";
+var google_conversion_color = "ffffff";
+var google_conversion_label = "KHBgCMyunQIQrJ3f6gM";
+var google_conversion_value = 0;
+/* ]]> */
+</script>
+<script type="text/javascript" src="http://www.googleadservices.com/pagead/conversion.js">
+</script>
+<noscript>
+<div style="display:inline;">
+<img height="1" width="1" style="border-style:none;" alt="" src="http://www.googleadservices.com/pagead/conversion/1029164716/?label=KHBgCMyunQIQrJ3f6gM&amp;guid=ON&amp;script=0"/>
+</div>
