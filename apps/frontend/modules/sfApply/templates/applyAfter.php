87d18f726470bf4d98ff4956c70b5dfca72236a9
Sergey Belov <limitium@users.noreply.github.com>
2011-10-27 13:28:12 +0000

    signup scenario

--- apps/frontend/modules/sfApply/templates/applyAfter.php
+++ apps/frontend/modules/sfApply/templates/applyAfter.php
@@ -1,8 +1,8 @@
 <div>
     <?php $mail = $form->getObject()->email; ?>
-    <?php $mailServer = 'www.' . substr($mail, strpos($mail, '@') + 1); ?>
+    <?php $mailServer = substr($mail, strpos($mail, '@') + 1); ?>
     <p>
-        Спасибо за регистрацию. Для подтверждения почтового адреса, Вам по электронной почте отправленно письмо. Проверьте свою почту - <a href="http://<?php echo $mailServer; ?>"><?php echo $mailServer; ?></a>.
+        Спасибо за регистрацию. Для подтверждения почтового адреса, Вам по электронной почте отправленно письмо. Проверьте свою почту - <a id="mail_server" href="http://<?php echo $mailServer; ?>">http://<?php echo $mailServer; ?></a>.
     </p>
     <?php include_partial('sfApply/continue') ?>
 </div>
