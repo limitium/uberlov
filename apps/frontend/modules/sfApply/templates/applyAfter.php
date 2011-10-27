22e8db23c103bee09baa896e817be3cc540ca2d0
limitium <limitium@50e93683-1401-4f79-b91e-9bcc27dc36b6>
2011-10-27 16:53:25 +0000

    fix continue

--- apps/frontend/modules/sfApply/templates/applyAfter.php
+++ apps/frontend/modules/sfApply/templates/applyAfter.php
@@ -4,5 +4,5 @@
     <p>
         Спасибо за регистрацию. Для подтверждения почтового адреса, Вам по электронной почте отправленно письмо. Проверьте свою почту - <a id="mail_server" href="http://<?php echo $mailServer; ?>">http://<?php echo $mailServer; ?></a>.
     </p>
-    <?php include_partial('sfApply/continue') ?>
+    <?php echo link_to(__("Continue",array(),'sfForkedApply'),  '@homepage') ?>
 </div>
