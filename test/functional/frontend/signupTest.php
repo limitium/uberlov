8df0f160ac0d4808e117ac1a8ee61f36f5eedc43
Sergey Belov <limitium@users.noreply.github.com>
2011-10-27 17:37:32 +0000

    signup scenario complete

--- test/functional/frontend/signupTest.php
+++ test/functional/frontend/signupTest.php
@@ -6,7 +6,7 @@ $browser = new sfTestFunctional(new sfBrowser());
 $test    = $browser->test();
 $conn    = Doctrine::getConnectionByTableName('location');
 
-//$conn->beginTransaction();
+$conn->beginTransaction();
 
 $browser
   ->call('/login', 'GET', array())
@@ -51,6 +51,10 @@ $browser
   ->with('request')->begin()
     ->isParameter('module', 'sfGuardAuth')
     ->isParameter('action', 'signin')
+  ->end()
+  ->info('user not registered')
+  ->with('form')->begin()
+    ->hasErrors(true)
   ->end()
   ->with('response')->begin()
     ->isStatusCode(200)
@@ -104,6 +108,7 @@ $browser
   ->end()
   ->with('response')->begin()
     ->isStatusCode(200)
+    ->info("user name limitium busy")
     ->matches("/false/")
   ->end()
 ;
@@ -122,6 +127,7 @@ $browser
   ->end()
   ->with('response')->begin()
     ->isStatusCode(200)
+    ->info("user name testuser free")
     ->matches("/true/")
   ->end()
 ;
@@ -141,6 +147,7 @@ $browser
   ->with('response')->begin()
     ->isStatusCode(200)
     ->matches("/false/")
+    ->info("email limitium busy")
   ->end()
 ;
 
@@ -158,6 +165,7 @@ $browser
   ->end()
   ->with('response')->begin()
     ->isStatusCode(200)
+    ->info("email testuser free")
     ->matches("/true/")
   ->end()
 ;
@@ -173,6 +181,7 @@ $browser->get('/user/new')
     'id' => '',
   ), array('_with_csrf' => true)
 )) 
+  ->info("create user")
   ->with('request')->begin()
     ->isParameter('module', 'sfApply')
     ->isParameter('action', 'apply')
@@ -182,6 +191,7 @@ $browser->get('/user/new')
   ->end()
   ->with('response')->begin()
     ->isStatusCode(200)
+  //->debug()
     ->checkElement('#mail_server')
   ->end();
   //Ошибка в процессе отправки почты. Пожалуйста, попробуйте еще раз позже. 
@@ -194,15 +204,17 @@ $browser
   ->end()
   ->with('response')->begin()
     ->isStatusCode(200)
+    ->matches("/является не правильным/")
   ->end()
 ;
 
-//$testuser = Doctrine_Query::create()->from('sfGuardUserProfile p')
-//        ->leftJoin("p.User u")
-//        ->where("u.username = 'testuser'")
-//        ->fetchOne();
+$testuser = Doctrine_Query::create()->from('sfGuardUserProfile p')
+        ->leftJoin("p.User u")
+        ->where("u.username = 'testuser'")
+        ->fetchOne();
 $browser
-  ->info($testuser->validate)
+  ->info("validate user:" . $testuser->getUser()->username)
+  ->info("validate key:".$testuser->validate)
   ->call('/user/confirm/'.$testuser->validate, 'GET', array())
   ->with('request')->begin()
     ->isParameter('module', 'sfApply')
@@ -210,6 +222,8 @@ $browser
   ->end()
   ->with('response')->begin()
     ->isStatusCode(200)
+    ->info("user validated!")
+    ->matches("/Теперь Вы вошли на сайт/")
   ->end()
 ;
 
@@ -246,18 +260,8 @@ $browser
 ;
 
 $browser
-  ->call('/sfJqueryReloadedPlugin/css/indicator.gif', 'GET', array())
-  ->with('request')->begin()
-    ->isParameter('module', 'default')
-    ->isParameter('action', 'error404')
-  ->end()
-  ->with('response')->begin()
-    ->isStatusCode(404)
-  ->end()
-;
-
-$browser
-  ->call('/profile/update/328', 'POST', array (
+  /*
+  ->call('/profile/update/'.$testuser->user_id, 'POST', array (
   'sf_medtod' => 'put',
   'profile' => 
   array (
@@ -268,12 +272,12 @@ $browser
     'description' => '
 <p>test</p>',
     'sex' => 'on',
-    'id' => '328',
+    'id' => $testuser->user_id,
   ),
-  'id' => '328',
-))
-  /*   ->get('/profile/update/328')
-  ->click('alt or value of submit here', array (
+  'id' => $testuser->user_id,
+))*/
+  ->get('/my_edit')
+  ->click('.button_01', array (
   'sf_medtod' => 'put',
   'profile' => 
   array (
@@ -284,19 +288,23 @@ $browser
     'description' => '
 <p>test</p>',
     'sex' => 'on',
-    'id' => '328',
+    'id' => $testuser->user_id,
   ),
-  'id' => '328',
-)) */ 
+  array('_with_csrf' => true),
+  'id' => $testuser->user_id,
+)) 
   ->with('request')->begin()
     ->isParameter('module', 'profile')
     ->isParameter('action', 'update')
   ->end()
 ;
 $browser
+  ->with('form')->begin()
+    ->hasErrors(false)
+  ->end()
   ->with('response')->begin()
     ->isRedirected(1)
-    ->isStatusCode(302)
+    ->isStatusCode(302)        
   ->end()
   ->followRedirect()
 ;
@@ -370,29 +378,33 @@ $browser
 ;
 
 $browser
-  ->call('/login', 'POST', array (
+  /*->call('/login', 'POST', array (
   'signin' => 
   array (
     'username' => 'testuser',
     'password' => 'testuser',
   ),
   'menu' => 'sf_guard_signin',
-))
-  /*   ->get('/login')
-  ->click('alt or value of submit here', array (
+))*/
+     ->get('/login')
+  ->click('.button_01', array (
   'signin' => 
   array (
     'username' => 'testuser',
     'password' => 'testuser',
   ),
+  array('_with_csrf' => true),
   'menu' => 'sf_guard_signin',
-)) */ 
+))  
   ->with('request')->begin()
     ->isParameter('module', 'sfGuardAuth')
     ->isParameter('action', 'signin')
   ->end()
 ;
 $browser
+  ->with('form')->begin()
+        ->hasErrors(false)
+  ->end()
   ->with('response')->begin()
     ->isRedirected(1)
     ->isStatusCode(302)
