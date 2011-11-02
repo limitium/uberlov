0063927ecf3ff4e428f3c7b4c78d8f18288292de
Sergey Belov <limitium@users.noreply.github.com>
2011-11-02 19:57:45 +0000

    w3c validations2

--- apps/frontend/modules/user/templates/_menu.php
+++ apps/frontend/modules/user/templates/_menu.php
@@ -1,11 +1,13 @@
-<ul id="user_menu">
-    <?php foreach ($menu as $menuItem): ?>
-        <li<?php echo substr($menuItem['url'], 1) == $sf_request->getParameter('menu') ? ' class="selected"' : '' ?>>
-            <a href="<?php echo url_for($menuItem['url']) ?>"><span class="label">
-                <span class="icon"><?php echo image_tag($menuItem['icon']); ?></span>
-                <strong><?php echo html_entity_decode($menuItem['title']); ?></strong>
-            </span>
-            </a>
-        </li>
-    <?php endforeach; ?>
-</ul>
+<?php if($menu): ?>
+    <ul id="user_menu">
+        <?php foreach ($menu as $menuItem): ?>
+            <li<?php echo substr($menuItem['url'], 1) == $sf_request->getParameter('menu') ? ' class="selected"' : '' ?>>
+                <a href="<?php echo url_for($menuItem['url']) ?>"><span class="label">
+                    <span class="icon"><?php echo image_tag($menuItem['icon']); ?></span>
+                    <strong><?php echo html_entity_decode($menuItem['title']); ?></strong>
+                </span>
+                </a>
+            </li>
+        <?php endforeach; ?>
+    </ul>
+<?php endif; ?>
