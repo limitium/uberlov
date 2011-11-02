92f2a978c56b9165896dbc52270386d1158c6452
limitium <limitium@50e93683-1401-4f79-b91e-9bcc27dc36b6>
2011-11-02 20:07:12 +0000

    w3c validations3

--- apps/frontend/modules/event/templates/_last.php
+++ apps/frontend/modules/event/templates/_last.php
@@ -1,15 +1,17 @@
-<div class="menu_block">
-    <h4>Ближайшие события:</h4>
-    <ul>
-        <?php foreach ($events as $event): ?>
-            <li>
-                <h5>
-                    <?php echo $event->getDateTimeObject('date')->format('d.m.Y') ?> <?php echo link_to($event->getName(), 'event/show?id=' . $event->getId()); ?>
-                </h5>
-                <span>
-                    <?php include_partial('profile/addBy', array('added' => $event,'size'=>24)); ?>
-                </span>
-            </li>
-        <?php endforeach; ?>
-    </ul>
-</div>
+<?php if($events->count()): ?>
+    <div class="menu_block">
+        <h4>Ближайшие события:</h4>
+        <ul>
+            <?php foreach ($events as $event): ?>
+                <li>
+                    <h5>
+                        <?php echo $event->getDateTimeObject('date')->format('d.m.Y') ?> <?php echo link_to($event->getName(), 'event/show?id=' . $event->getId()); ?>
+                    </h5>
+                    <span>
+                        <?php include_partial('profile/addBy', array('added' => $event,'size'=>24)); ?>
+                    </span>
+                </li>
+            <?php endforeach; ?>
+        </ul>
+    </div>
+<?php endif; ?>
