130565f05aa12e8fbba7f32760e5e224168cbe59
Sergey Belov <limitium@users.noreply.github.com>
2011-11-13 17:42:11 +0000

    fixed top list

--- apps/frontend/modules/profile/actions/actions.class.php
+++ apps/frontend/modules/profile/actions/actions.class.php
@@ -12,12 +12,12 @@ class profileActions extends sfActions {
 
     public function executeCity(sfWebRequest $request) {
         $this->cities = Doctrine_Query::create()
-                ->from('City c')
-                ->where('c.name LIKE ?', $request->getParameter('q') . '%')
-                ->leftJoin('c.Region')
-                ->limit($request->getParameter('limit', 10))
-                ->orderBy('c.weight desc, c.name desc')
-                ->execute();
+                        ->from('City c')
+                        ->where('c.name LIKE ?', $request->getParameter('q') . '%')
+                        ->leftJoin('c.Region')
+                        ->limit($request->getParameter('limit', 10))
+                        ->orderBy('c.weight desc, c.name desc')
+                        ->execute();
     }
 
     public function executeList(sfWebRequest $request) {
@@ -33,17 +33,18 @@ class profileActions extends sfActions {
 
     public function executeTopProfit(sfWebRequest $request) {
         $peopleOrdered = array();
-        foreach (Doctrine_Query::create()
-                ->select('sum(pd.qty) as weight,p.created_by as id')
-                ->from('Profit p')
-                ->leftJoin('p.ProfitDetail pd')
-                ->groupBy('p.created_by')
-                ->orderBy('weight desc')
-                ->limit(10)
-                ->execute() as $loc) {
-            $peopleOrdered[$loc->id] = $loc->weight;
+        $ps = Doctrine_Query::create()
+                        ->select('sum(pd.qty) as weight,p.created_by as id')
+                        ->from('ProfitDetail pd')
+                        ->leftJoin('pd.Profit p')
+                        ->groupBy('p.created_by')
+                        ->orderBy('weight desc')
+                        ->limit(10)
+                        ->execute(array(), Doctrine::HYDRATE_NONE);
+        foreach ($ps as $p) {
+            $peopleOrdered[$p[1]] = $p[0];
         }
-
+        
         foreach (Doctrine_Query::create()
                 ->from('sfGuardUserProfile p')
                 ->leftJoin('p.User u')
@@ -63,6 +64,7 @@ class profileActions extends sfActions {
         $request->setParameter('id', $this->getUser()->getProfile()->getId());
         $this->forward('profile', 'show');
     }
+
     public function executeEditMy(sfWebRequest $request) {
         $request->setParameter('id', $this->getUser()->getProfile()->getId());
         $this->forward('profile', 'edit');
@@ -103,41 +105,41 @@ class profileActions extends sfActions {
         if (!in_array($this->view, array('comments', 'profits', 'locations'))) {
             $this->view = 'profile';
         }
-        
-        
+
+
         //@todo: join commmented objects
         $this->comments = Doctrine_Query::create()
-                ->from('Comment c')
-                ->leftJoin('c.CreatedBy')                
-                ->where('c.created_by = ? and c.parent > 0', $this->profile->getId())
-                ->andWhere('c.inbox_id is null')
-                ->orderBy('c.created_at desc');
+                        ->from('Comment c')
+                        ->leftJoin('c.CreatedBy')
+                        ->where('c.created_by = ? and c.parent > 0', $this->profile->getId())
+                        ->andWhere('c.inbox_id is null')
+                        ->orderBy('c.created_at desc');
 
         $this->comments = Doctrine::getTable('Comment')->filterScope($this->comments, $this->getUser())->execute();
         $this->profits = Doctrine_Query::create()
-                ->from('Profit p')
-                ->leftJoin('p.ProfitDetail d')
-                ->leftJoin('p.Location l')
-                ->leftJoin('d.Fish f')
-                ->leftJoin('d.Style s')
-                ->leftJoin('p.CommentProfit')
-                ->leftJoin('p.VoteProfit')
-                ->leftJoin('p.CreatedBy')
-                ->andWhere('p.created_by = ?', $this->profile->getId())
-                ->orderBy('p.created_at desc');
+                        ->from('Profit p')
+                        ->leftJoin('p.ProfitDetail d')
+                        ->leftJoin('p.Location l')
+                        ->leftJoin('d.Fish f')
+                        ->leftJoin('d.Style s')
+                        ->leftJoin('p.CommentProfit')
+                        ->leftJoin('p.VoteProfit')
+                        ->leftJoin('p.CreatedBy')
+                        ->andWhere('p.created_by = ?', $this->profile->getId())
+                        ->orderBy('p.created_at desc');
 
         $this->profits = Doctrine::getTable('Location')
-                ->filterScope($this->profits, $this->getUser())->execute();
+                        ->filterScope($this->profits, $this->getUser())->execute();
 
         $this->locations = Doctrine::getTable('Location')
-                ->getVisibleLocationsQuery($this->getUser())
-                ->leftJoin('l.VoteLocation v')
-                ->leftJoin('l.CreatedBy p')
-                ->leftJoin('l.CommentLocation c')
-                ->leftJoin('l.Profit pr')
-                ->andWhere('l.created_by = ?', $this->profile->getId())
-                ->orderBy('l.created_at desc')
-                ->execute();
+                        ->getVisibleLocationsQuery($this->getUser())
+                        ->leftJoin('l.VoteLocation v')
+                        ->leftJoin('l.CreatedBy p')
+                        ->leftJoin('l.CommentLocation c')
+                        ->leftJoin('l.Profit pr')
+                        ->andWhere('l.created_by = ?', $this->profile->getId())
+                        ->orderBy('l.created_at desc')
+                        ->execute();
 //@todo: add events
         $this->total = 0;
         $this->best = array(
